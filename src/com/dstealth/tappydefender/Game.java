7fb42e9440ffc62d8ac954071afc6fa50fb83283
DalBrar <31530959+DalBrar@users.noreply.github.com>
2019-10-09 22:04:50 -0600

    Refactored long functions in Game class to many smaller functions for easier readability and code understanding.

--- src/com/dstealth/tappydefender/Game.java
+++ src/com/dstealth/tappydefender/Game.java
@@ -122,38 +122,7 @@ public class Game extends Canvas implements Runnable {
 	public void run() {
 		// run main menu screen
 		while (this.isMainMenu) {
-			updateFlashTime();
-			
-			BufferStrategy bs = this.getBufferStrategy();
-			if (bs == null) {
-				this.createBufferStrategy(3);
-				continue;
-			}
-			
-			Graphics g = bs.getDrawGraphics();
-			g.setColor(Color.black);
-			g.fillRect(0, 0, this.getWidth(), this.getHeight());
-			
-			BufferedImage bg = SpriteSheet.createImageFromResource("background.jpg");
-			Image img = bg.getScaledInstance(this.screenX, this.screenY, Image.SCALE_DEFAULT);
-			g.drawImage(img, 0, 0, null);
-
-	    	final int fontSizeL = 40;
-	    	final int fontSizeS = 18;
-	    	final int fontSizeT = 14;
-			Font fontL = new Font("consolas", 1, fontSizeL);
-			Font fontS = new Font("consolas", 1, fontSizeS);
-			Font fontT = new Font("consolas", 1, fontSizeT);
-			g.setColor(new Color(255, 255, 255, 200));
-			
-			if (this.flashTime <= 5)
-				drawStringCenter(g, fontL, "Press ENTER to Start", 180);
-			drawStringCenter(g, fontS, "CONTROLS:", 370);
-			drawStringCenter(g, fontT, "SPACE = Fly/Boost", 390);
-			drawStringCenter(g, fontT, "ENTER = Pause    ", 405);
-			
-			g.dispose();
-			bs.show();
+			drawMainMenu();
 		}
 		// run game screens
         while(!this.isMainMenu && this.playing) {
@@ -186,71 +155,19 @@ public class Game extends Canvas implements Runnable {
     	this.isPaused = (this.isPaused) ? false : true;
         this.timeStarted = System.currentTimeMillis();
     }
+    
+    // ==================================================
+    //		Game Loop Functions
+    // ==================================================
 
 	// tick method
 	private void update() {
 		// update Game if game not paused
 		if (!this.isPaused) {
-			// Check for collisions
-	        if (Rect.intersects(this.player.hitbox, this.enemy1.hitbox)) {
-	        	this.enemy1.x = -500; 
-	        	this.collision = true;
-	        }
-	        if (Rect.intersects(this.player.hitbox, this.enemy2.hitbox)) {
-	        	this.enemy2.x = -500;
-	        	this.collision = true;
-	        }
-	        if (Rect.intersects(this.player.hitbox, this.enemy3.hitbox)) {
-	        	this.enemy3.x = -500;
-	        	this.collision = true;
-	        }
-	        if (Rect.intersects(this.player.hitbox, this.enemy4.hitbox)) {
-	        	this.enemy4.x = -500;
-	        	this.collision = true;
-	        }
-	
-	        if (!this.gameEnded && this.collision) {
-	            this.player.strength--;
-	            this.s_bump.play();
-	        }
-	        if (!this.gameEnded) {
-	            // Update the player
-	            this.player.update();
-	            // Update the enemies
-	            this.enemy1.update(this.player.speed);
-	            this.enemy2.update(this.player.speed);
-	            this.enemy3.update(this.player.speed);
-	            this.enemy4.update(this.player.speed);
-	        }
-	        // Update space dust
-	        for (SpaceDust dust : this.dustList) {
-	            dust.update(this.player.speed);
-	        }
-
-	        if (!this.gameEnded) {
-	            // subtract distance to home planet based on current speed
-	            this.distanceRemaining -= this.player.speed;
-	            // How long has the player been flying?
-	            this.timeTaken += System.currentTimeMillis() - this.timeStarted;
-	            this.timeStarted = System.currentTimeMillis();
-	        }
-	        if (this.distanceRemaining < 0) {
-	            this.s_win.play();
-	            // check for new fastest time
-	            if (this.timeTaken < this.fastestTime) {
-	                this.fastestTime = this.timeTaken;
-	                // save new high score
-	                //editor.putLong(fastestStr, fastestTime);
-	                //editor.commit();
-	            }
-
-	            //avoid ugly negative numbers in the HUD
-	            this.distanceRemaining = 0;
-
-	            // end the game
-	            this.gameEnded = true;
-	            this.player.boosting = false;
-	        }
+			updateCollisions();
+			updateGameObjects();
+			updateHUD();
+	        updateVictoryCondition();
 		}
 	}
 	
@@ -263,7 +180,150 @@ public class Game extends Canvas implements Runnable {
 		}
 		
 		Graphics g = bs.getDrawGraphics();
+
+		drawBackground(g);
+		drawGameObjects(g);
+        drawHUDs(g);
 		
+		g.dispose();
+		bs.show();
+	}
+	
+	// thread speed control method
+	private void control() {
+        if (!this.gameEnded && this.player.strength < 0) {
+            this.gameEnded = true;
+            this.player.shipWrecked();
+            this.s_destroyed.play();
+        }
+        try {
+            Thread.sleep(17);
+        } catch (InterruptedException e) {
+
+        }
+	}
+
+    // ==================================================
+    //		Update Functions
+    // ==================================================
+	
+	private void updateCollisions() {
+        if (Rect.intersects(this.player.hitbox, this.enemy1.hitbox)) {
+        	this.enemy1.x = -500; 
+        	this.collision = true;
+        }
+        if (Rect.intersects(this.player.hitbox, this.enemy2.hitbox)) {
+        	this.enemy2.x = -500;
+        	this.collision = true;
+        }
+        if (Rect.intersects(this.player.hitbox, this.enemy3.hitbox)) {
+        	this.enemy3.x = -500;
+        	this.collision = true;
+        }
+        if (Rect.intersects(this.player.hitbox, this.enemy4.hitbox)) {
+        	this.enemy4.x = -500;
+        	this.collision = true;
+        }
+
+        if (!this.gameEnded && this.collision) {
+            this.player.strength--;
+            this.s_bump.play();
+            System.out.println("taking a hit");
+        }
+	}
+	
+	private void updateGameObjects() {
+        if (!this.gameEnded) {
+            // Update the player
+            this.player.update();
+            // Update the enemies
+            this.enemy1.update(this.player.speed);
+            this.enemy2.update(this.player.speed);
+            this.enemy3.update(this.player.speed);
+            this.enemy4.update(this.player.speed);
+        }
+        // Update space dust
+        for (SpaceDust dust : this.dustList) {
+            dust.update(this.player.speed);
+        }
+	}
+	
+	private void updateHUD() {
+		if (!this.gameEnded) {
+            // subtract distance to home planet based on current speed
+            this.distanceRemaining -= this.player.speed;
+            // How long has the player been flying?
+            this.timeTaken += System.currentTimeMillis() - this.timeStarted;
+            this.timeStarted = System.currentTimeMillis();
+        }
+	}
+	
+	private void updateVictoryCondition() {
+		if (this.distanceRemaining < 0) {
+            this.s_win.play();
+            // check for new fastest time
+            if (this.timeTaken < this.fastestTime) {
+                this.fastestTime = this.timeTaken;
+                // save new high score
+                //editor.putLong(fastestStr, fastestTime);
+                //editor.commit();
+            }
+
+            //avoid ugly negative numbers in the HUD
+            this.distanceRemaining = 0;
+
+            // end the game
+            this.gameEnded = true;
+            this.player.boosting = false;
+        }
+	}
+    
+    private void updateFlashTime() {
+    	this.flashTime++;
+    	if (this.flashTime >= 10)
+    		this.flashTime = 0;
+    }
+
+    // ==================================================
+    //		Draw Functions
+    // ==================================================
+	
+	private void drawMainMenu() {
+		updateFlashTime();
+		
+		BufferStrategy bs = this.getBufferStrategy();
+		if (bs == null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+		
+		Graphics g = bs.getDrawGraphics();
+		g.setColor(Color.black);
+		g.fillRect(0, 0, this.getWidth(), this.getHeight());
+		
+		BufferedImage bg = SpriteSheet.createImageFromResource("background.jpg");
+		Image img = bg.getScaledInstance(this.screenX, this.screenY, Image.SCALE_DEFAULT);
+		g.drawImage(img, 0, 0, null);
+
+    	final int fontSizeL = 40;
+    	final int fontSizeS = 18;
+    	final int fontSizeT = 14;
+		Font fontL = new Font("consolas", 1, fontSizeL);
+		Font fontS = new Font("consolas", 1, fontSizeS);
+		Font fontT = new Font("consolas", 1, fontSizeT);
+		g.setColor(new Color(255, 255, 255, 200));
+		
+		if (this.flashTime <= 5)
+			drawStringCenter(g, fontL, "Press ENTER to Start", 180);
+		drawStringCenter(g, fontS, "CONTROLS:", 370);
+		drawStringCenter(g, fontT, "SPACE = Fly/Boost", 390);
+		drawStringCenter(g, fontT, "ENTER = Pause    ", 405);
+		
+		g.dispose();
+		bs.show();
+	}
+	
+	private void drawBackground(Graphics g) {
 		// draw red background if collision else black background
 		if (!this.gameEnded && this.collision) {
 			g.setColor(Color.red);
@@ -273,7 +333,9 @@ public class Game extends Canvas implements Runnable {
 			g.setColor(Color.black);
 			g.fillRect(0, 0, this.getWidth(), this.getHeight());
 		}
-		
+	}
+	
+	private void drawGameObjects(Graphics g) {
 		// draw space dust
 		g.setColor(Color.white);
         for (SpaceDust dust : this.dustList) {
@@ -294,88 +356,93 @@ public class Game extends Canvas implements Runnable {
             g.drawImage(this.enemy3.bitmap, this.enemy3.x, this.enemy3.y, null);
             g.drawImage(this.enemy4.bitmap, this.enemy4.x, this.enemy4.y, null);
         }
+	}
 
-        // Draw appropriate HUD
+    // Draw appropriate HUD
+	private void drawHUDs(Graphics g) {
         if (!this.gameEnded) {
-        	// Gameplay HUD
-        	final int fontSize = 15;
-    		Font font = new Font("consolas", 1, fontSize);
-    		g.setFont(font);
-
-    		// Top text
-    		if (this.player.strength > 1)
-    			g.setColor(new Color(0, 255, 0, 225));
-    		else
-        	if (this.player.strength == 1)
-        		g.setColor(new Color(255, 255, 0, 225));
-        	else
-        		g.setColor(new Color(255, 0, 0, 225));
-    		g.drawString("Shields: " + this.player.strength, 5, fontSize);
-    		
-    		g.setColor(new Color(150, 150, 150, 225));
-    		drawStringCenter(g, font, "Time: " + formatTime(this.timeTaken), fontSize);
-    		g.drawString("Fastest: " + formatTime(this.fastestTime), this.screenX - 130, fontSize);
-    		// bottom text
-    		drawStringCenter(g, font, "Distance: " + formatDistance(this.distanceRemaining), this.screenY - fontSize);
-    		g.drawString("Speed: " + formatSpeed(this.player.speed), this.screenX - 130, this.screenY - fontSize);
+        	drawGameplayHUD(g);
+            if (this.isPaused)
+            	drawPauseHUD(g);
         }
-        else {
-        	// Game Over Screen
-        	final int fontSize1 = 25;
-        	final int fontSize2 = 60;
-    		Font fontS = new Font("consolas", 1, fontSize1);
-    		Font fontL = new Font("consolas", 1, fontSize2);
-    		g.setColor(new Color(255, 255, 255, 200));
-    		
-    		drawStringCenter(g, fontL, "Game Over", fontSize2);
-    		drawStringCenter(g, fontS, "Fastest Time: " + formatTime(this.fastestTime), 100);
-    		drawStringCenter(g, fontS, "This Time: " + formatTime(this.timeTaken), 130);
-    		if (this.distanceRemaining == 0) {
-        		g.setColor(new Color(0, 255, 0, 200));
-    			drawStringCenter(g, fontS, "Made It Home!", 160);
-    		} else {
-        		g.setColor(new Color(255, 0, 0, 200));
-    			drawStringCenter(g, fontS, "Distance Remaining: " + formatDistance(this.distanceRemaining), 160);
-    		}
-    		
-    		g.setColor(new Color(255, 255, 255, 200));
-    		drawStringCenter(g, fontS, "Press ENTER to replay!", 250);
-        }
-        
-    	// if paused then draw Pause HUD
-        if (this.isPaused) {
-			updateFlashTime();
-        	
-	    	final int fontSizeS = 25;
-	    	final int fontSizeL = 60;
-			Font fontS = new Font("consolas", 1, fontSizeS);
-			Font fontL = new Font("consolas", 1, fontSizeL);
-			g.setColor(new Color(255, 255, 255, 200));
-			
-			if (this.flashTime <= 5)
-				drawStringCenter(g, fontL, "Paused", fontSizeL+50);
-			drawStringCenter(g, fontS, "Press ENTER to resume", 180);
-			drawStringCenter(g, fontS, "Press ESC to restart", 240);
-        }
-		
-		g.dispose();
-		bs.show();
+        else
+        	drawGameOverScreen(g);
 	}
 	
-	// thread speed control method
-	private void control() {
-        if (!this.gameEnded && this.player.strength < 0) {
-            this.gameEnded = true;
-            this.player.shipWrecked();
-            this.s_destroyed.play();
-        }
-        try {
-            Thread.sleep(17);
-        } catch (InterruptedException e) {
+	private void drawGameplayHUD(Graphics g) {
+    	final int fontSize = 15;
+		Font font = new Font("consolas", 1, fontSize);
+		g.setFont(font);
 
-        }
+		// Top text
+		if (this.player.strength > 1)
+			g.setColor(new Color(0, 255, 0, 225));
+		else
+    	if (this.player.strength == 1)
+    		g.setColor(new Color(255, 255, 0, 225));
+    	else
+    		g.setColor(new Color(255, 0, 0, 225));
+		g.drawString("Shields: " + this.player.strength, 5, fontSize);
+		
+		g.setColor(new Color(150, 150, 150, 225));
+		drawStringCenter(g, font, "Time: " + formatTime(this.timeTaken), fontSize);
+		g.drawString("Fastest: " + formatTime(this.fastestTime), this.screenX - 130, fontSize);
+		// bottom text
+		drawStringCenter(g, font, "Distance: " + formatDistance(this.distanceRemaining), this.screenY - fontSize);
+		g.drawString("Speed: " + formatSpeed(this.player.speed), this.screenX - 130, this.screenY - fontSize);
 	}
+	
+	private void drawGameOverScreen(Graphics g) {
+    	final int fontSize1 = 25;
+    	final int fontSize2 = 60;
+		Font fontS = new Font("consolas", 1, fontSize1);
+		Font fontL = new Font("consolas", 1, fontSize2);
+		g.setColor(new Color(255, 255, 255, 200));
+		
+		drawStringCenter(g, fontL, "Game Over", fontSize2);
+		drawStringCenter(g, fontS, "Fastest Time: " + formatTime(this.fastestTime), 100);
+		drawStringCenter(g, fontS, "This Time: " + formatTime(this.timeTaken), 130);
+		if (this.distanceRemaining == 0) {
+    		g.setColor(new Color(0, 255, 0, 200));
+			drawStringCenter(g, fontS, "Made It Home!", 160);
+		} else {
+    		g.setColor(new Color(255, 0, 0, 200));
+			drawStringCenter(g, fontS, "Distance Remaining: " + formatDistance(this.distanceRemaining), 160);
+		}
+		
+		g.setColor(new Color(255, 255, 255, 200));
+		drawStringCenter(g, fontS, "Press ENTER to replay!", 250);
+	}
+	
+	private void drawPauseHUD(Graphics g) {
+		updateFlashTime();
+    	
+    	final int fontSizeS = 25;
+    	final int fontSizeL = 60;
+		Font fontS = new Font("consolas", 1, fontSizeS);
+		Font fontL = new Font("consolas", 1, fontSizeL);
+		g.setColor(new Color(255, 255, 255, 200));
+		
+		if (this.flashTime <= 5)
+			drawStringCenter(g, fontL, "Paused", fontSizeL+50);
+		drawStringCenter(g, fontS, "Press ENTER to resume", 180);
+		drawStringCenter(g, fontS, "Press ESC to restart", 240);
+	}
+    
+    private int getStringCenter(Graphics g, Font font, String str) {
+    	FontMetrics metrics = g.getFontMetrics(font);
+    	return (this.screenX - metrics.stringWidth(str)) / 2;
+    }
+    
+    private void drawStringCenter(Graphics g, Font font, String str, int y) {
+		g.setFont(font);
+		g.drawString(str, getStringCenter(g, font, str), y);
+    }
 
+    // ==================================================
+    //		Helper Functions
+    // ==================================================
+	
     private static String formatTime(long time) {
         int sec = (int) (time / 1000);
         int dec = (int) ((time % 1000)/100);
@@ -393,21 +460,5 @@ public class Game extends Canvas implements Runnable {
     	double multiplier = ((totalDistance / avgTimeToComplete) * 10);
     	double speed = ((int)(shipspeed * multiplier))/10.0;
     	return speed + " km/s";
-    }
-    
-    private int getStringCenter(Graphics g, Font font, String str) {
-    	FontMetrics metrics = g.getFontMetrics(font);
-    	return (this.screenX - metrics.stringWidth(str)) / 2;
-    }
-    
-    private void drawStringCenter(Graphics g, Font font, String str, int y) {
-		g.setFont(font);
-		g.drawString(str, getStringCenter(g, font, str), y);
-    }
-    
-    private void updateFlashTime() {
-    	this.flashTime++;
-    	if (this.flashTime >= 10)
-    		this.flashTime = 0;
     }
 }
